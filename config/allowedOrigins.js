d77e0f83eef725f7c6b37ddc6122d46804f02524
AbdullahSeraj <106870849+AbdullahSeraj@users.noreply.github.com>
2025-05-13 20:43:51 +0300

    Changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,8 @@
 const webstie = process.env.WEBSITE_URL;
-const allowedOrigins = ["http://localhost:3000", webstie];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://dxn-new-front-end.onrender.com",
+  webstie,
+];
 
 module.exports = allowedOrigins;
